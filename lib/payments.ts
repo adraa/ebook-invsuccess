45175d89103b2899d798edf97201d4f1a01148af
Dheepan Periasamy <dheepan.periasamy@gmail.com>
2025-11-14 15:30:24 +0800

    Fix Stripe API version compatibility issue

--- lib/payments.ts
+++ lib/payments.ts
@@ -4,9 +4,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('STRIPE_SECRET_KEY is not set')
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-06-20',
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export interface CheckoutSessionData {
   paymentMethod: 'stripe' | 'paypal'
